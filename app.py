80d0196aa14caef423d39b133b57cca3cdf8f225
trevorwitter <trevorgwitter@gmail.com>
2017-06-04 13:53:42 -0400

    Added about page to app.py

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def index():
   return render_template('index.html')
 
+@app.route('/about')
+def about():
+  return render_template('index.html')
+
 if __name__ == '__main__':
   app.run(port=33507)
